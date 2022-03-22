e718aabe726854a9fd86954bca01713b8aab5d21
MaximovAS <MaximovAlSer@mpei.ru>
2022-03-22 18:35:15 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
 int main()
 {
     cout << "Enter A and B: ";
-int a, b;
+int a, b, s;
 cin >> a >> b;
+s= a+b;
+cout << s;
     return 0;
 }
