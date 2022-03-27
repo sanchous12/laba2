b07588229c9eafdf3bb429e2d090a604774310f7
MaximovAS <MaximovAlSer@mpei.ru>
2022-03-28 00:22:35 +0300

    code: заготовка программы

--- main.cpp
+++ main.cpp
@@ -5,11 +5,17 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-int a, b, s,k;
+int a, b;
 cin >> a >> b;
 cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
      << "A / B = " << a / b << '\n';
+if (a>b){
+cout << "max=" << a;
+}
+else{
+cout << "max=" << b;
+}
     return 0;
 }
